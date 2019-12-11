db487dd62298a597855124f120907dcff3df8335
Prashant1503 <prashant1998vani@gmail.com>
2019-12-11 09:58:38 +0530

    initial commit

---
+++ reactSampleApp/components/HelloWorld.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {Text, View, Image} from 'react-native';
+
+class CustomCard extends Component {
+  render() {
+    return (
+      <View style={{alignContent: 'center'}}>
+        <Text>{this.props.name}</Text>
+        <Text>{this.props.age}</Text>
+        <Image
+          source={{
+            uri:
+              'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
+          }}
+          style={{width: 200, height: 200}}
+        />
+      </View>
+    );
+  }
+} //Default class..
+
+export default class Custom extends Component {
+  render() {
+    return (
+      <View>
+        <CustomCard name="Prashant" age="21" />
+      </View>
+    );
+  }
+}
